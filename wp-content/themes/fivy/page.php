a300ae88e8366b55c530578fb743d901409a0277
annguyenit <annguyenphp@gmail.com>
2015-08-17 00:29:44 +0700

    Add scream page + update db

---
+++ wp-content/themes/fivy/page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : ?>
+<?php while ( have_posts() ) : the_post(); ?>
+<section class="scream">
+    <div class="container">
+      <!--======= TITTLE =========-->
+      <div class="tittle">
+        <h1><?php echo get_the_title(); //page tilte filter?></h1>
+      </div>
+	  <?php the_content(); ?>
+      <div class="fb-comments" data-href="http://www.getdeals.nl/schreeuw-en-win/" data-width="675" data-num-posts="10"></div>
+    </div>
+</section>
+<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
